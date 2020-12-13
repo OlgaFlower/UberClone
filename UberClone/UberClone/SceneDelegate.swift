808371b2e17dca47a364efaa25c25177844e6a95
Olga <kindzyadzyu@gmail.com>
2020-12-13 18:37:49 +0200

    update scene delegate

--- UberClone/UberClone/SceneDelegate.swift
+++ UberClone/UberClone/SceneDelegate.swift
@@ -15,6 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
+        FirebaseApp.configure()
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = UINavigationController(rootViewController: LoginController())
