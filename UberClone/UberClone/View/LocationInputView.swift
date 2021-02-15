1995df0455a81c20cba7196fd5ccd48a093dd146
Olga <kindzyadzyu@gmail.com>
2021-02-15 14:45:19 +0200

    add location input view ui wrap up

--- UberClone/UberClone/View/LocationInputView.swift
+++ UberClone/UberClone/View/LocationInputView.swift
@@ -5,4 +5,43 @@
 //  Created by Olha Bereziuk on 15.02.2021.
 //
 
-import Foundation
+import UIKit
+
+protocol LocationInputViewDelegate: class {
+    func dismissLocationInputView()
+}
+
+class LocationInputView: UIView {
+    
+    // MARK: - Properties
+    weak var delegate: LocationInputViewDelegate?
+    
+    private let backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage( #imageLiteral(resourceName: "baseline_arrow_back_black_36dp-1").withRenderingMode(.alwaysOriginal), for: .normal) //prevent default blue tint
+        button.addTarget(self, action: #selector(handleBackTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addShadow()
+        backgroundColor = .white
+        
+        addSubview(backButton)
+        backButton.anchor(top: topAnchor, left: leftAnchor,
+                          paddingTop: 44, paddingLeft: 12,
+                          width: 24, height: 25)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    @objc func handleBackTapped() {
+        delegate?.dismissLocationInputView()
+    }
+}
