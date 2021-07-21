e7a54368e9580f016b1916698dcffcc9ada3c183
Olga <kindzyadzyu@gmail.com>
2021-07-21 13:08:41 +0300

    add side menu controller

---
+++ UberClone/UberClone/Controllers/SideMenu/ContainerController.swift
@@ -0,0 +1,8 @@
+//
+//  ContainerController.swift
+//  UberClone
+//
+//  Created by Olha Bereziuk on 21.07.2021.
+//
+
+import Foundation
---
+++ UberClone/UberClone/Controllers/SideMenu/MenuController.swift
@@ -0,0 +1,8 @@
+//
+//  MenuController.swift
+//  UberClone
+//
+//  Created by Olha Bereziuk on 21.07.2021.
+//
+
+import Foundation
